f75abef962273f82353ca317fa18fef4f7db7d5d
root <root@lamp.(none)>
2014-08-22 17:46:56 +0000

    Changed url of support panel.

--- htdocs/help.php
+++ htdocs/help.php
@@ -10,4 +10,4 @@ API::add('User','getInfo',array($_SESSION['session_id']));
 $query = API::send();
 $user_info = $query['User']['getInfo']['results'][0];
 
-Link::redirect('http://1btcxe.freshdesk.com/login/sso?name='.urlencode($user_info['first_name'].' '.$user_info['last_name']).'&email='.urlencode($user_info['email']).'&amp;timestamp='.(time()).'&hash='.hash_hmac('md5',($user_info['first_name'].' '.$user_info['last_name'].$user_info['email'].(time())),$CFG->freshdesk_key));
+Link::redirect('http://support.1btcxe.com/login/sso?name='.urlencode($user_info['first_name'].' '.$user_info['last_name']).'&email='.urlencode($user_info['email']).'&amp;timestamp='.(time()).'&hash='.hash_hmac('md5',($user_info['first_name'].' '.$user_info['last_name'].$user_info['email'].(time())),$CFG->freshdesk_key));
