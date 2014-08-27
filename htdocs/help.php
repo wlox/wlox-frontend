dcc5c1a61e2648396cee9d3a4a2395861850459c
root <root@lamp.(none)>
2014-08-27 18:40:35 +0000

    N

--- htdocs/help.php
+++ htdocs/help.php
@@ -12,7 +12,7 @@ $user_info = $query['User']['getInfo']['results'][0];
 $remote_url = preg_replace("/[^a-z0-9\/-]/", "",$_REQUEST['url']);
 
 if ($remote_url) {
-	$remote_url = '&redirect_to='.urlencode('https://1btcxe.freshdesk.com/'.$remote_url);
+	$remote_url = '&redirect_to='.urlencode('https://support.1btcxe.com/'.$remote_url);
 }
 
 Link::redirect('http://support.1btcxe.com/login/sso?name='.urlencode($user_info['first_name'].' '.$user_info['last_name']).'&email='.urlencode($user_info['email']).'&amp;timestamp='.(time()).'&hash='.hash_hmac('md5',($user_info['first_name'].' '.$user_info['last_name'].$user_info['email'].(time())),$CFG->freshdesk_key).$remote_url);
