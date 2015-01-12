9dabbdba70cef6b90b4f5ce8e7ecfa0eb1bbe290
root <moises@organic.com.pa>
2015-01-12 17:39:47 +0000

    Moved password regex to backend config, fixed a few minor issues.

--- htdocs/login.php
+++ htdocs/login.php
@@ -7,7 +7,7 @@ if (stristr($_SERVER["SERVER_NAME"],'www.'))
 
 $page_title = Lang::string('home-login');
 $user1 = (!empty($_REQUEST['login']['user'])) ? preg_replace("/[^0-9]/", "",$_REQUEST['login']['user']) : false;
-$pass1 = (!empty($_REQUEST['login']['pass'])) ? preg_replace("/[^0-9a-zA-Z!@#$%&*?\.\-\_]/", "",$_REQUEST['login']['pass']) : false;
+$pass1 = (!empty($_REQUEST['login']['pass'])) ? preg_replace($CFG->pass_regex, "",$_REQUEST['login']['pass']) : false;
 
 if (!empty($_REQUEST['submitted'])) {
 	if (empty($user1)) {
